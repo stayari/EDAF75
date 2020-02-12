28f2c3ad38586842ffb910ece5516951a051d6cb
Sepehr Tayari <sepehr@Sepehrs-MacBook-Pro.local>
2020-02-12 11:55:13 +0100

    remade keys in database and added jupyter file

--- Laborations/lab2/lab2.sql
+++ Laborations/lab2/lab2.sql
@@ -3,24 +3,34 @@ DROP TABLE IF EXISTS theaters
 CREATE TABLE theaters(
 	th_name 	TEXT,
 	capacity	INT,
-	PRIMARY KEY (name)
+	PRIMARY KEY (th_name)
 ) 
 -- Osäker
 DROP TABLE IF EXISTS movies
 CREATE TABLE movies(
-	IMDB_key	TEXT, --?? 
+	IMDB_key	TEXT, 
 	name		TEXT,
 	year		INT,
 	duration    INT, 
-	start_time	TEXT,
-	PRIMARY KEY (name, start_time)
+	--start_time	TEXT,
+	PRIMARY KEY (IMDB_key)
 ) 
+
+DROP TABLE IF EXISTS shows(
+CREATE TABLE shows(
+	IMDB_key	TEXT,
+	th_name		TEXT,
+	start_time	TEXT,
+
+	PRIMARY KEY(IMDB_key, start_time, th_name)
+	)
+)
 
 DROP TABLE IF EXISTS customers
 CREATE TABLE customers(
 	user_id		TEXT,
 	full_name	TEXT,
-	password 	TEXT
+	password 	TEXT, -- hasha
 	PRIMARY KEY (user_id)
 ) 
 
@@ -31,10 +41,9 @@ CREATE TABLE tickets(
 	IMDB_key	TEXT,
 	start_time  TEXT,
 
-	PRIMARY KEY (ticket_id, th_name, IMDB_key, start_time),
-	FOREIGN KEY (th_name) REFERENCES theaters(th_name)
-	FOREIGN KEY (IMDB_key, start_time) REFERENCES movies(IMDB_key, start_time)
+	PRIMARY KEY (ticket_id)
+	--FOREIGN KEY (th_name) REFERENCES theaters(th_name)
+	--FOREIGN KEY (IMDB_key, start_time) REFERENCES movies(IMDB_key, start_time)
 	--FOREIGN KEY (start_time) REFERENCES movies(start_time)
-
-
 ) 
+
