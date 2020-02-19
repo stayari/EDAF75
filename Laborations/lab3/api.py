4e246bb59398ed66c7aa8c9e42cf44e456355768
Fabian Frankel <fa7320fr-s@student.lu.se>
2020-02-19 16:06:13 +0100

    added ping function to API

--- Laborations/lab3/api.py
+++ Laborations/lab3/api.py
@@ -5,18 +5,11 @@ import json
 
 conn = sqlite3.connect("applications.sqlite")
 
-@get('/students')
-def get_students():
-    c = conn.cursor()
-    c.execute(
-        """
-        SELECT s_id, s_name, gpa, size_hs
-        FROM   students
-        """
-    )
-    s = [{"id": id, "name": name, "gpa": gpa, "hsSize": hs_size}
-         for (id, name, gpa, hs_size) in c]
-    return json.dumps({"data": s}, indent=4)
+@get('/ping')
+def return_pong():
+    return "pong"
+
+
 
 
 run(host='localhost', port=7007)
