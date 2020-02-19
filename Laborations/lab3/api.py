db9e02029b847b8749d5e6d1e07993be73c4560a
Fabian Frankel <fa7320fr-s@student.lu.se>
2020-02-19 16:45:48 +0100

    added methods movies and customers to api

--- Laborations/lab3/api.py
+++ Laborations/lab3/api.py
@@ -3,12 +3,40 @@ from bottle import run
 import sqlite3
 import json
 
-conn = sqlite3.connect("applications.sqlite")
+conn = sqlite3.connect("movies.sqlite")
 
 @get('/ping')
 def return_pong():
     return "pong"
 
+
+
+@get('/movies')
+def return_movies():
+    c = conn.cursor()  
+    c.execute(
+        """
+        SELECT *
+        FROM   movies
+        """
+    )
+    s = [{"IMDB_KEY": IMDB_KEY, "name": name, "year": year, "duration": duration}
+         for (IMDB_KEY, name, year, duration) in c]
+    return json.dumps({"data": s}, indent=4)
+
+
+@get('/customers')
+def return_customers():
+    c = conn.cursor()  
+    c.execute(
+        """
+        SELECT *
+        FROM customers   
+        """
+    )
+    d = [{"user_name": user_name, "full_name": full_name, "password": password}
+         for (user_name, full_name, password) in c]
+    return json.dumps({"data": d}, indent=4)
 
 
 
